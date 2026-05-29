2bb06c016d51b55208db0c83edc8b0b4a91bea3e
Sam <103283741+salamandarin@users.noreply.github.com>
2026-05-29 16:40:06 -0500

    fixed mobile header bar

--- scripts/general_scripts.js
+++ scripts/general_scripts.js
@@ -1,3 +1,4 @@
+// mobile nav bar hamburger button
 function hamburger() {
     let menu = document.getElementById("nav_links");
     if(menu.style.display === "block") {
@@ -6,4 +7,19 @@ function hamburger() {
     else {
       menu.style.display = "block";
     }
+
+    updateHeaderHeight()
 }
+
+
+// calculate header height
+function updateHeaderHeight() {
+  const header = document.querySelector('header');
+  document.documentElement.style.setProperty('--header-height', `${header.offsetHeight}px`);
+
+  console.log(header.offsetHeight)
+}
+
+document.addEventListener('DOMContentLoaded', updateHeaderHeight);
+window.addEventListener('load', updateHeaderHeight);
+window.addEventListener('resize', updateHeaderHeight);
